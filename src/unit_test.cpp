ba1bc70359fec201dcf6ebb28bebca60be49cd35
Kian Behzad <kian.behzad@gmail.com>
2022-03-09 14:16:52 +0330

    add unit tests

--- src/unit_test.cpp
+++ src/unit_test.cpp
@@ -1,13 +1,150 @@
 
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "server.h"
+#include "client.h"
 
-// Will be completed gradually, use
-// git pull command to update your
-// local repository
 
 TEST(HW1Test, TEST1) {
-    EXPECT_EQ(2, 2);
+    Server server{};
+    auto bryan{server.add_client("bryan")};
+    EXPECT_EQ(bryan->get_id(), "bryan");
+}
+
+TEST(HW1Test, TEST2) {
+    Server server{};
+    auto bryan{server.add_client("bryan")};
+    auto bryan_from_server{server.get_client("bryan")};
+    EXPECT_EQ(bryan.get(), bryan_from_server.get());
+}
+
+TEST(HW1Test, TEST3) {
+    Server server{};
+    auto bryan{server.add_client("bryan")};
+    EXPECT_DOUBLE_EQ(bryan->get_wallet(), 5.0);
+}
+
+TEST(HW1Test, TEST4) {
+    Server server{};
+    auto bryan1{server.add_client("bryan")};
+    auto bryan2{server.add_client("bryan")};
+    auto bryan3{server.add_client("bryan")};
+    EXPECT_NE(bryan1->get_id(), bryan2->get_id());
+    EXPECT_NE(bryan1->get_id(), bryan3->get_id());
+    EXPECT_NE(bryan2->get_id(), bryan3->get_id());
+}
+
+TEST(HW1Test, TEST5) {
+    Server server{};
+    auto bryan{server.add_client("bryan")};
+    std::string public_key{bryan->get_publickey()};
+    EXPECT_TRUE(!bryan->get_publickey().empty());
+}
+
+TEST(HW1Test, TEST6) {
+    Server server{};
+    auto bryan{server.add_client("bryan")};
+    auto clint{server.add_client("clint")};
+    EXPECT_TRUE(bryan->get_publickey() != clint->get_publickey());
+}
+
+TEST(HW1Test, TEST7) {
+    Server server{};
+    auto bryan{server.add_client("bryan")};
+    auto clint{server.add_client("clint")};
+    Server const* p{&server};
+    auto client = p->get_client("no_one");
+    EXPECT_TRUE(client == nullptr);
+}
+
+TEST(HW1Test, TEST8) {
+    Server server{};
+    auto bryan{server.add_client("bryan")};
+    auto clint{server.add_client("clint")};
+    show_wallets(server);
+}
+
+TEST(HW1Test, TEST9) {
+    Server server{};
+    auto bryan{server.add_client("bryan")};
+    Client const* p{bryan.get()};
+    std::string signature{p->sign("mydata")};
+    EXPECT_TRUE(crypto::verifySignature(p->get_publickey(), "mydata", signature));
+    EXPECT_FALSE(crypto::verifySignature(p->get_publickey(), "notmydata", signature));
+    EXPECT_FALSE(crypto::verifySignature(p->get_publickey(), "mydata", "not_my_signature"));
+}
+
+TEST(HW1Test, TEST10) {
+    std::string sender{}, receiver{};
+    double value;
+    Server::parse_trx("sarah-clay-0.5", sender, receiver, value);
+    EXPECT_EQ(sender, "sarah");
+    EXPECT_EQ(receiver, "clay");
+    EXPECT_DOUBLE_EQ(value, 0.5);
+}
+
+TEST(HW1Test, TEST11) {
+    std::string sender{}, receiver{};
+    double value;
+    EXPECT_THROW(Server::parse_trx("sarah-clay_0.5", sender, receiver, value), std::runtime_error);
+}
+
+TEST(HW1Test, TEST12) {
+    Server server{};
+    auto bryan{server.add_client("bryan")};
+    auto clint{server.add_client("clint")};
+    bool valid{bryan->transfer_money("no_one", 0.5)};
+    EXPECT_FALSE(valid);
+}
+
+TEST(HW1Test, TEST13) {
+    Server server{};
+    auto bryan{server.add_client("bryan")};
+    auto clint{server.add_client("clint")};
+    bool valid{bryan->transfer_money("clint", 100)};
+    EXPECT_FALSE(valid);
+}
+
+TEST(HW1Test, TEST14) {
+    Server server{};
+    pending_trxs.clear();
+    auto bryan{server.add_client("bryan")};
+    auto clint{server.add_client("clint")};
+    auto sarah{server.add_client("sarah")};
+    EXPECT_TRUE(bryan->transfer_money("clint", 1));
+    EXPECT_TRUE(clint->transfer_money("sarah", 2.5));
+    EXPECT_TRUE(sarah->transfer_money("bryan", 0.5));
+
+    std::cout  <<  std::string(20, '*') <<  std::endl;
+    for(const  auto& trx : pending_trxs)
+        std::cout << trx <<  std::endl;
+    std::cout  <<  std::string(20, '*') <<  std::endl;
+}
+
+TEST(HW1Test, TEST15) {
+    Server server{};
+    pending_trxs.clear();
+    auto bryan{server.add_client("bryan")};
+    auto clint{server.add_client("clint")};
+    auto sarah{server.add_client("sarah")};
+    EXPECT_TRUE(bryan->transfer_money("clint", 1));
+    EXPECT_TRUE(clint->transfer_money("sarah", 2.5));
+    EXPECT_TRUE(sarah->transfer_money("bryan", 0.5));
+
+    std::string mempool{};
+    for(const auto& trx : pending_trxs)
+        mempool += trx;
+        
+    show_wallets(server);
+    size_t nonce{server.mine()};
+    show_wallets(server);
+
+    std::string hash = crypto::sha256(mempool + std::to_string(nonce));
+    EXPECT_TRUE(hash.substr(0, 10).find("000") != std::string::npos);
+    // MINER is: sarah || bryan || clint
+    EXPECT_TRUE(bryan->get_wallet()==4.5 || bryan->get_wallet()==10.75 || bryan->get_wallet()==4.5);
+    EXPECT_TRUE(clint->get_wallet()==3.5 ||clint->get_wallet()==3.5 ||clint->get_wallet()==9.75);
+    EXPECT_TRUE(sarah->get_wallet()==13.25 || sarah->get_wallet()==7 || sarah->get_wallet()==7);
 }
 
 
